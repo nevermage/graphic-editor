d3afc9f64ffd66ba5c9bc384484fe68e65f36037
nevermage <maksim14032002@gmail.com>
2021-11-22 12:24:50 +0200

    save img

--- Public/index.php
+++ Public/index.php
@@ -15,51 +15,57 @@
         </div>
         <input id="imgLoader" name="inputImg" type="file" onchange="previewImg(event)">
     </div>
-    <div class="addFigures">
-        <select id="selectFigure" onchange="figureForm()">
-            <option value="1">квадрат</option>
-            <option value="2">прямоугольник</option>
-            <option value="3">параллелограмм</option>
-            <option value="4">овал</option>
-            <option value="5">окружность</option>
-            <option value="6">точка</option>
-            <option value="7">отрезок</option>
-            <option value="8">треугольник</option>
-            <option value="9">текст</option>
-        </select>
-        <div class="figureForm" id="figureForm1" style="display: block;">
-            <p>точка 1</p>
-            <input name="zxc2" type="number">
-            <input type="number">
-            <p>точка 2</p>
-            <input type="number">
-            <input type="number">
-            <p>точка 3</p>
-            <input type="number">
-            <input type="number">
-            <p>точка 4</p>
-            <input type="number">
-            <input type="number">
-            <br>
-            <button onclick="">add figure</button>
-<!--            <input type="submit" value="Загрузить">-->
+    <div class="rightBox">
+        <div class="addFigures">
+            <select id="selectFigure" onchange="figureForm()">
+                <option value="1">квадрат</option>
+                <option value="2">прямоугольник</option>
+                <option value="3">параллелограмм</option>
+                <option value="4">овал</option>
+                <option value="5">окружность</option>
+                <option value="6">точка</option>
+                <option value="7">отрезок</option>
+                <option value="8">треугольник</option>
+                <option value="9">текст</option>
+            </select>
+            <div class="figureForm" id="figureForm1" style="display: block;">
+                <p>точка 1</p>
+                <input name="zxc2" type="number">
+                <input type="number">
+                <p>точка 2</p>
+                <input type="number">
+                <input type="number">
+                <p>точка 3</p>
+                <input type="number">
+                <input type="number">
+                <p>точка 4</p>
+                <input type="number">
+                <input type="number">
+                <br>
+                <button onclick="">add figure</button>
+                <!--            <input type="submit" value="Загрузить">-->
+            </div>
+            <div class="figureForm" id="figureForm2">
+                <p>точка 1</p>
+                <input type="number">
+                <input type="number">
+                <p>точка 2</p>
+                <input type="number">
+                <input type="number">
+                <p>точка 3</p>
+                <input type="number">
+                <input type="number">
+                <br>
+                <!--            <input type="submit" value="Загрузить">-->
+            </div>
         </div>
-        <div class="figureForm" id="figureForm2">
-            <p>точка 1</p>
-            <input type="number">
-            <input type="number">
-            <p>точка 2</p>
-            <input type="number">
-            <input type="number">
-            <p>точка 3</p>
-            <input type="number">
-            <input type="number">
+        <div class="authorForm">
+            <p>Ваше имя:</p>
+            <input type="text" name="author" class="authorInput">
             <br>
-<!--            <input type="submit" value="Загрузить">-->
+            <button onclick="uploadImage()">Загрузить</button>
         </div>
-
     </div>
-    <button onclick="uploadImage()">load</button>
 </form>
 </body>
 </html>
--- Public/upload.php
+++ Public/upload.php
@@ -2,18 +2,28 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//$dsn = 'mysql:dbname=testTask;host=127.0.0.1';
-//$user = 'root';
-//$password = 'root';
-//
-//$pdo = new PDO($dsn, $user, $password);
-//$stmt = $pdo->query("select * from images");
+//header("Content-type: image/png");
+
+$files = scandir('img/', SCANDIR_SORT_DESCENDING);
+$newest_file = $files[0];
+if ($newest_file === '..') {
+    $l = 1;
+} else {
+    $l = intval(pathinfo($newest_file, PATHINFO_FILENAME)) + 1;
+}
+$newPng = "img/$l.png";
+
+$dsn = 'mysql:dbname=testTask;host=127.0.0.1';
+$user = 'root';
+$password = 'root';
+$pdo = new PDO($dsn, $user, $password);
+$sql = "INSERT INTO `testTask`.`images` (`author`, `image`, `date`) 
+        VALUES ('$newPng', '" . $_POST['author'] . "', date(curdate()));";
+$stmt = $pdo->query($sql);
+
 //$result = $stmt->fetchAll();
 //var_dump($result);
 
-//print_r($_FILES['inputImg']);
-
-header("Content-type: image/png");
 $im = imagecreatefrompng($_FILES['inputImg']['tmp_name']);
-imagepng($im);
-var_dump($im);
+imagepng($im, $newPng);
+imagedestroy($im);
